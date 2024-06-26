bfe9bf13589f622c437f7f56968d069a6a6f0113
UgurkanTech <ugurkanstr@gmail.com>
2024-06-26 09:49:15 +0300

    Update ui.py

--- src/examples/ui.py
+++ src/examples/ui.py
@@ -1,8 +1,34 @@
 import streamlit as st
 import datetime
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from NetNode import *
+
+class BotHandler:
+    def __init__(self):
+        self.model = NetModelFactory.createModel(NetModelType.CHAT, model="phi3")
+        self.options = Options(temperature=1, top_k=64, top_p=0.9, repeat_penalty=1.2, stream=False)
+        self.model.setOptions(self.options)
+        self.context = ChatContext(self.model.options['num_ctx'], self.model.options['num_predict'])
+        self.context.setSystemPrompt(self.model.options['system_prompt'])
+        self.history = ChatHistory()
+
+    def get_response(self, user_message):
+        self.history.push_message(ChatMessage(Sender.USER, user_message))
+        self.context.setHistory(self.history)
+        self.context.updateContext()
+        self.model.createModelResponse(self.context.context.getContextDict())
+        response = self.model.getResponse(ResultType.STRING)
+        self.history.push_message(ChatMessage(Sender.ASSISTANT, response))
+        return response
+
+if 'bot_handler' not in st.session_state:
+    st.session_state.bot_handler = BotHandler()
 
 def get_bot_response(user_message):
-    return f"Echo back: {user_message}"
+    return st.session_state.bot_handler.get_response(user_message)
 
 # Sidebar
 with st.sidebar:
